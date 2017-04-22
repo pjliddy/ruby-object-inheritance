d6e766f495a5276340e7a28d7992b11341c32e7a
pjliddy <pjliddy@gmail.com>
2017-04-22 10:47:34 -0400

    Finished code along for inheritance
    Finished code along for inheritance

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -1,3 +1,15 @@
 # frozen_string_literal: true
 
 require_relative 'shape'
+
+class Rectangle < Shape
+  def initialize(side_length_one, side_length_two)
+    @side_length_one = side_length_one
+    @side_length_two = side_length_two
+    @num_sides = 4
+  end
+
+  def calculate_area
+    @side_length_one * @side_length_two
+  end
+end
